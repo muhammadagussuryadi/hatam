983636a8dcd11099506a0ab208fa0ab99a51182a
muhammad agus suryadi <muhammadagussuryadiii@gmail.com>
2021-12-22 19:24:46 +0700

    testing app

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,53 @@
 function appJs(){
   $(document).ready(function(){
-    !function(n){"use strict";function a(){n(window).width()<1300?n("body").addClass("enlarge-menu enlarge-menu-all"):n("body").removeClass("enlarge-menu enlarge-menu-all")}!function(){if(0!=n("#Dash_Date").length){var i=n("#Dash_Date"),a=moment(),t=moment();i.daterangepicker({startDate:a,endDate:t,opens:"left",applyClass:"btn btn-sm btn-primary",cancelClass:"btn btn-sm btn-secondary",ranges:{Today:[moment(),moment()],Yesterday:[moment().subtract(1,"days"),moment().subtract(1,"days")],"Last 7 Days":[moment().subtract(6,"days"),moment()],"Last 30 Days":[moment().subtract(29,"days"),moment()],"This Month":[moment().startOf("month"),moment().endOf("month")],"Last Month":[moment().subtract(1,"month").startOf("month"),moment().subtract(1,"month").endOf("month")]}},e),e(a,t,"")}function e(a,t,e){var n="",s="";t-a<100||"Today"==e?(n="Today:",s=a.format("MMM D")):"Yesterday"==e?(n="Yesterday:",s=a.format("MMM D")):s=a.format("MMM D")+" - "+t.format("MMM D"),i.find("#Select_date").html(s),i.find("#Day_Name").html(n)}}(),n(".metismenu").metisMenu(),n(window).resize(function(){a()}),n(".button-menu-mobile").on("click",function(a){a.preventDefault(),n("body").toggleClass("enlarge-menu")}),a(),n(".main-icon-menu .nav-link").on("click",function(a){n("body").removeClass("enlarge-menu"),a.preventDefault(),n(this).addClass("active"),n(this).siblings().removeClass("active"),n(".main-menu-inner").addClass("active");var t=n(this).attr("href");n(t).addClass("active"),n(t).siblings().removeClass("active")}),n(".leftbar-tab-menu a, .left-sidenav a").each(function(){var a=window.location.href.split(/[?#]/)[0];if(this.href==a){n(this).addClass("active"),n(this).parent().addClass("active"),n(this).parent().parent().addClass("in"),n(this).parent().parent().addClass("mm-show"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().prev().addClass("active"),n(this).parent().parent().parent().addClass("active"),n(this).parent().parent().parent().parent().addClass("mm-show"),n(this).parent().parent().parent().parent().parent().addClass("mm-active");var t=n(this).closest(".main-icon-menu-pane").attr("id");n("a[href='#"+t+"']").addClass("active")}}),feather.replace(),n(".navigation-menu a").each(function(){var a=window.location.href.split(/[?#]/)[0];this.href==a&&(n(this).parent().addClass("active"),n(this).parent().parent().parent().addClass("active"),n(this).parent().parent().parent().parent().parent().addClass("active"))}),n(".navbar-toggle").on("click",function(a){n(this).toggleClass("open"),n("#navigation").slideToggle(400)}),n(".navigation-menu>li").slice(-2).addClass("last-elements"),n('.navigation-menu li.has-submenu a[href="#"]').on("click",function(a){n(window).width()<992&&(a.preventDefault(),n(this).parent("li").toggleClass("open").find(".submenu:first").toggleClass("open"))}),Waves.init()}(jQuery);
+    // !function(n){"use strict";function a(){n(window).width()<1300?n("body").addClass("enlarge-menu enlarge-menu-all"):n("body").removeClass("enlarge-menu enlarge-menu-all")}!function(){if(0!=n("#Dash_Date").length){var i=n("#Dash_Date"),a=moment(),t=moment();i.daterangepicker({startDate:a,endDate:t,opens:"left",applyClass:"btn btn-sm btn-primary",cancelClass:"btn btn-sm btn-secondary",ranges:{Today:[moment(),moment()],Yesterday:[moment().subtract(1,"days"),moment().subtract(1,"days")],"Last 7 Days":[moment().subtract(6,"days"),moment()],"Last 30 Days":[moment().subtract(29,"days"),moment()],"This Month":[moment().startOf("month"),moment().endOf("month")],"Last Month":[moment().subtract(1,"month").startOf("month"),moment().subtract(1,"month").endOf("month")]}},e),e(a,t,"")}function e(a,t,e){var n="",s="";t-a<100||"Today"==e?(n="Today:",s=a.format("MMM D")):"Yesterday"==e?(n="Yesterday:",s=a.format("MMM D")):s=a.format("MMM D")+" - "+t.format("MMM D"),i.find("#Select_date").html(s),i.find("#Day_Name").html(n)}}(),n(".metismenu").metisMenu(),n(window).resize(function(){a()}),n(".button-menu-mobile").on("click",function(a){a.preventDefault(),n("body").toggleClass("enlarge-menu")}),a(),n(".main-icon-menu .nav-link").on("click",function(a){n("body").removeClass("enlarge-menu"),a.preventDefault(),n(this).addClass("mm-active"),n(this).siblings().removeClass("mm-active"),n(".main-menu-inner").addClass("mm-active");var t=n(this).attr("href");n(t).addClass("mm-active"),n(t).siblings().removeClass("mm-active")}),n(".leftbar-tab-menu a, .left-sidenav a").each(function(){var a=window.location.href.split(/[?#]/)[0];if(this.href==a){n(this).addClass("mm-active"),n(this).parent().addClass("mm-active"),n(this).parent().parent().addClass("in"),n(this).parent().parent().addClass("mm-show"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().prev().addClass("mm-active"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().parent().parent().addClass("mm-show"),n(this).parent().parent().parent().parent().parent().addClass("mm-active");var t=n(this).closest(".main-icon-menu-pane").attr("id");n("a[href='#"+t+"']").addClass("mm-active")}}),feather.replace(),n(".navigation-menu a").each(function(){var a=window.location.href.split(/[?#]/)[0];this.href==a&&(n(this).parent().addClass("mm-active"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().parent().parent().parent().addClass("mm-active"))}),n(".navbar-toggle").on("click",function(a){n(this).toggleClass("open"),n("#navigation").slideToggle(400)}),n(".navigation-menu>li").slice(-2).addClass("last-elements"),n('.navigation-menu li.has-submenu a[href="#"]').on("click",function(a){n(window).width()<992&&(a.preventDefault(),n(this).parent("li").toggleClass("open").find(".submenu:first").toggleClass("open"))}),Waves.init()}(jQuery);
+		!function(n){"use strict";function a(){n(window).width()<1300?n("body").addClass("enlarge-menu enlarge-menu-all"):n("body").removeClass("enlarge-menu enlarge-menu-all")}!function(){if(0!=n("#Dash_Date").length){var i=n("#Dash_Date"),a=moment(),t=moment();i.daterangepicker({startDate:a,endDate:t,opens:"left",applyClass:"btn btn-sm btn-primary",cancelClass:"btn btn-sm btn-secondary",ranges:{Today:[moment(),moment()],Yesterday:[moment().subtract(1,"days"),moment().subtract(1,"days")],"Last 7 Days":[moment().subtract(6,"days"),moment()],"Last 30 Days":[moment().subtract(29,"days"),moment()],"This Month":[moment().startOf("month"),moment().endOf("month")],"Last Month":[moment().subtract(1,"month").startOf("month"),moment().subtract(1,"month").endOf("month")]}},e),e(a,t,"")}function e(a,t,e){var n="",s="";t-a<100||"Today"==e?(n="Today:",s=a.format("MMM D")):"Yesterday"==e?(n="Yesterday:",s=a.format("MMM D")):s=a.format("MMM D")+" - "+t.format("MMM D"),i.find("#Select_date").html(s),i.find("#Day_Name").html(n)}}(),n(".metismenu").metisMenu(),n(window).resize(function(){a()}),n(".button-menu-mobile").on("click",function(a){a.preventDefault(),n("body").toggleClass("enlarge-menu")}),a(),n(".main-icon-menu .nav-link").on("click",function(a){n("body").removeClass("enlarge-menu"),a.preventDefault(),n(this).addClass("mm-active"),n(this).siblings().removeClass("mm-active"),n(".main-menu-inner").addClass("mm-active");var t=n(this).attr("href");n(t).addClass("mm-active"),n(t).siblings().removeClass("mm-active")}),n(".leftbar-tab-menu a, .left-sidenav a").each(function(){var a=window.location.href.split(/[?#]/)[0];if(this.href==a){n(this).addClass("mm-active"),n(this).parent().addClass("mm-active"),n(this).parent().parent().addClass("in"),n(this).parent().parent().addClass("mm-show"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().prev().addClass("mm-active"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().parent().parent().addClass("mm-show"),n(this).parent().parent().parent().parent().parent().addClass("mm-active");var t=n(this).closest(".main-icon-menu-pane").attr("id");n("a[href='#"+t+"']").addClass("mm-active")}}),feather.replace(),n(".navigation-menu a").each(function(){var a=window.location.href.split(/[?#]/)[0];this.href==a&&(n(this).parent().addClass("mm-active"),n(this).parent().parent().parent().addClass("mm-active"),n(this).parent().parent().parent().parent().parent().addClass("mm-active"))}),n(".navbar-toggle").on("click",function(a){n(this).toggleClass("open"),n("#navigation").slideToggle(400)}),n(".navigation-menu>li").slice(-2).addClass("last-elements"),n('.navigation-menu li.has-submenu a[href="#"]').on("click",function(a){n(window).width()<992&&(a.preventDefault(),n(this).parent("li").toggleClass("open").find(".submenu:first").toggleClass("open"))}),Waves.init()}(jQuery);
   });
+}
+
+function printDiv(){
+  // $(".text-export").html('being processed...');
+	var divContents = document.getElementById("pdfVoucher").innerHTML;
+	var element = '<html>'+
+									+'<style>table{width:100%!important;}</style>'
+									+'<body>'
+										+'<div style="font-size:12px;padding: 10px;">'
+										+divContents
+										+'</div>'
+									+'</body>'
+								+'</html>';
+	var opt = {
+			margin: 0.1,
+			filename: "voucher-hatam.pdf",
+			enableLinks: !1,
+			pagebreak: { mode: ['avoid-all', 'css', 'legacy'] },
+			image: {
+					type: "jpeg",
+					quality: 0.98
+			},
+			html2canvas: {
+					scale: 2,
+          dpi: 300,
+          letterRendering: true,
+          useCORS: true
+			},
+			jsPDF: {
+					unit: "in",
+					format: "a4",
+					orientation: "portrait"
+					// orientation: "landscape"
+			}
+	};
+	html2pdf().from(element).set(opt).toPdf().get("pdf").then(function(pdf) {
+			var totalPages = pdf.internal.getNumberOfPages();
+			// console.log("getHeight:" + pdf.internal.pageSize.getHeight());
+			// console.log("getWidth:" + pdf.internal.pageSize.getWidth());
+			for (var i = 1; i <= totalPages; i++) {
+					pdf.setPage(i);
+					pdf.setFontSize(10);
+					pdf.setTextColor(150);
+					// pdf.text("Page " + i + " of " + totalPages, pdf.internal.pageSize.getWidth() / 2, pdf.internal.pageSize.getHeight() / 2)
+			}
+			// $(".text-export").html("");
+	}).save()
 }
